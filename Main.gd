31004cd052b229d3e3d9d474884c1b3600b02846
Phillyp Henning <phillyp.henning@gmail.com>
2024-06-27 22:35:40 -0400

    Adding HUD and gameplay loop

--- Main.gd
+++ Main.gd
@@ -14,11 +14,17 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready..")
+	
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
@@ -40,6 +46,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
